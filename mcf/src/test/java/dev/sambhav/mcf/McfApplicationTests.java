57a594d8d4781a6ef17e3d2d1b885dfb2993b554
Jigar <95428432+jigarsiddhpura@users.noreply.github.com>
2024-11-20 00:46:51 +0530

    initialize spring

---
+++ mcf/src/test/java/dev/sambhav/mcf/McfApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.sambhav.mcf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
